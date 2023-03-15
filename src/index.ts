a7d3ae258fbdb31a4827f1ba126668868bcd9da6
Casenrique <carloshenriquesouza.eng@gmail.com>
2023-03-15 15:21:00 -0300

    Adicionado exemplo no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { userRouter } from './router/userRouter'
 import { playlistRouter } from './router/playlistRouter'
+import { multerUpload } from './multer'
+import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
+import { bucketName, s3 } from './s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 dotenv.config()
 
@@ -17,3 +21,37 @@ app.listen(Number(process.env.PORT), () => {
 
 app.use("/users", userRouter)
 app.use("/playlists", playlistRouter)
+
+app.post("/files", multerUpload.single('uploadedFile'), async (req: Request, res: Response) => {
+    try {
+      const uploadedFile = req.file
+  
+      if (uploadedFile == undefined) {
+        throw new Error("'uploadedFile' é obrigatório")
+      }
+  
+      const uploadedFileKey = `${Math.ceil(Math.random() * 20)}-${uploadedFile.originalname}`
+  
+      await s3.send(new PutObjectCommand({
+        Bucket: bucketName,
+        Body: uploadedFile.buffer,
+        Key: uploadedFileKey,
+        ContentType: uploadedFile.mimetype
+      }))
+  
+      const url = await getSignedUrl(
+        s3,
+        new GetObjectCommand({
+          Bucket: bucketName,
+          Key: uploadedFileKey
+        }),
+        { expiresIn: 120 }
+      )
+  
+      res.send({ url })
+    } catch (error) {
+      if (error instanceof Error) {
+        res.send(error.message)
+      }
+    }
+  })
